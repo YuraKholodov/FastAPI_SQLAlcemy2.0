3233b7e9fa1ba1a87225fc89f7d26c0aa34f0295
Yura Kholodov <yurdast@mail.ru>
2024-01-25 15:17:08 +0300

    25.01.24

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from pydantic import EmailStr, BaseModel
 import uvicorn
 
+from core.config import settings
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
 
-@app.get('/')
+@app.get("/")
 def hello_index():
-    return {
-        'message': 'Hello index!'
-    }
+    return {"message": "Hello index!"}
 
 
-@app.get('/hello/')  # /hello/?name=Yura
-def hello(name: str = 'World'):
+@app.get("/hello/")  # /hello/?name=Yura
+def hello(name: str = "World"):
     name = name.strip().title()
-    return {'message': f'Hello {name}'}
+    return {"message": f"Hello {name}"}
 
 
-@app.post('/calc/add/')
+@app.post("/calc/add/")
 def add(a: int, b: int):
     return {
-        'a': a,
-        'b': b,
-        'result': a + b,
+        "a": a,
+        "b": b,
+        "result": a + b,
     }
 
 
