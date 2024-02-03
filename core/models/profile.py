1061a6c59a4815f1e128746d3e8ba81d51bcd51a
Yura Kholodov <yurdast@mail.ru>
2024-02-03 11:18:23 +0300

    create profile and user_relation_mixin

--- core/models/profile.py
+++ core/models/profile.py
@@ -1,20 +1,16 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-
-# Что-бы небыло циклического импорта, импортируем User только при проверки типов
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Profile(Base):
+class Profile(UserRelationMixin, Base):
     __tablename__ = "profiles"
+
+    _user_id_unique = True
+    _user_back_populates = "profile"
 
     first_name: Mapped[str | None] = mapped_column(String(40))
     last_name: Mapped[str | None] = mapped_column(String(40))
     bio: Mapped[str | None]
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
--- core/models/user.py
+++ core/models/user.py
@@ -8,6 +8,7 @@ from .base import Base
 # Что-бы небыло циклического импорта, импортируем Post только при проверки типов
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 
 class User(Base):
@@ -16,3 +17,4 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(30), unique=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
